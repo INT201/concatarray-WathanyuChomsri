2ee8a8ac42c1e299008ef57312d8cd83768890d6
WathanyuChomsri <wathanyu.chom@kmutt.ac.th>
2022-09-08 02:16:12 +0700

    ex4

--- concatArray.js
+++ concatArray.js
@@ -1,9 +1,9 @@
-//const { template } = require('@babel/core')
+const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
   //code here
-  array1=[]
-  array2=[]
+  // array1=[5, 10, 15]
+  // array2=[2, 4, 6, 7]
   if(array1 && array2 == null || undefined){
     return undefined
   }else if (array1 && array2 == []){
@@ -15,4 +15,4 @@ function concatArray(array1, array2) {
   return concatArray(array1, array2)
 }
 
-//module.exports = concatArray
+module.exports = concatArray
