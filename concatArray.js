e705d7c4991d310568c65fb7bc3a1c6fc825600a
WathanyuChomsri <wathanyu.chom@kmutt.ac.th>
2022-09-28 21:56:22 +0700

    concat

--- concatArray.js
+++ concatArray.js
@@ -1,23 +1,23 @@
- const { template } = require('@babel/core')
+const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
 
 
-  if ((array1 == null || array1 == undefined) && (array2 == null|| array2 == undefined )) {
+  if ((array1 === null || array1 === undefined) && (array2 === null|| array2 === undefined )) {
     return undefined
-
-  } else if (array1 == [] && array2 == []) {
+  
+  } else if (array1?.length === 0 && array2?.length === 0) {
     return array1
 
-  } else if (array1 == [] || array1==null || array1==undefined) {
+  } else if (array1?.length ===0 || array1===null || array1===undefined) {
     return array2
 
-  } else if (array2 == [] || array2 == null || array2 ==undefined) {
+  } else if (array2?.length ===0 || array2 === null || array2 ===undefined) {
     return array1
 
-  } else {
+  } 
     return array1.concat(array2)
-  }
+  
 }
 
 
@@ -33,4 +33,4 @@ function concatArray(array1, array2) {
 // console.log(concatArray(undefined, null))
 // console.log(concatArray(null, null))
 
-module.exports = concatArray
+ module.exports = concatArray
